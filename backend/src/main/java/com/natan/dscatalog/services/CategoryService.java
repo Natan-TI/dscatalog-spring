cd36bacda42967ee09252356eae47dc63fdfba40
Natan-TI <natan201125@hotmail.com>
2023-02-13 20:55:18 -0300

    Update category

--- backend/src/main/java/com/natan/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/natan/dscatalog/services/CategoryService.java
@@ -11,7 +11,9 @@ import org.springframework.transaction.annotation.Transactional;
 import com.natan.dscatalog.dto.CategoryDTO;
 import com.natan.dscatalog.entities.Category;
 import com.natan.dscatalog.repositories.CategoryRepository;
-import com.natan.dscatalog.services.exceptions.EntityNotFoundException;
+import com.natan.dscatalog.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 
 
@@ -30,7 +32,7 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
 		return new CategoryDTO(entity);
 	}
 	
@@ -40,5 +42,18 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDTO(entity);
+	}
+
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		try {
+			Category entity = repository.getReferenceById(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDTO(entity);
+		} catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found! Id:" + id);
+		}
+		
 	}
 }
