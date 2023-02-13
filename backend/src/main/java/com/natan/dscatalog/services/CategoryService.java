e71519d0fe549f075803dc1383dcab8d288f3590
Natan-TI <natan201125@hotmail.com>
2023-02-13 03:39:40 -0300

    h2 database

---
+++ backend/src/main/java/com/natan/dscatalog/services/CategoryService.java
@@ -0,0 +1,20 @@
+package com.natan.dscatalog.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.natan.dscatalog.entities.Category;
+import com.natan.dscatalog.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+}
