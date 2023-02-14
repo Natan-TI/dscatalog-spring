f0a142b2e554ed559e5a7784701f55f59b06fe6b
Natan-TI <natan201125@hotmail.com>
2023-02-14 11:01:06 -0300

    Delete category

--- backend/src/main/java/com/natan/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/natan/dscatalog/services/CategoryService.java
@@ -5,12 +5,15 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.natan.dscatalog.dto.CategoryDTO;
 import com.natan.dscatalog.entities.Category;
 import com.natan.dscatalog.repositories.CategoryRepository;
+import com.natan.dscatalog.services.exceptions.DatabaseException;
 import com.natan.dscatalog.services.exceptions.ResourceNotFoundException;
 
 import jakarta.persistence.EntityNotFoundException;
@@ -55,5 +58,15 @@ public class CategoryService {
 			throw new ResourceNotFoundException("Id not found! Id:" + id);
 		}
 		
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found! Id:" + id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 }
